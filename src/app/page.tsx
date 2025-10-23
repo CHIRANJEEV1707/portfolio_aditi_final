f7b3a5cb3c4f5e121a925f6c1b2cf90bb908bde8
Chiranjeev <chiatbits@gmail.com>
2025-10-23 14:51:02 +0000

    remove My Skillz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from '@/components/sections/HeroSection';
 import AboutSection from '@/components/sections/AboutSection';
-import SkillsSection from '@/components/sections/SkillsSection';
 import ProjectsSection from '@/components/sections/ProjectsSection';
 import ContactSection from '@/components/sections/ContactSection';
 import { projects } from '@/lib/data';
@@ -10,7 +9,6 @@ export default function Home() {
     <>
       <HeroSection />
       <AboutSection />
-      <SkillsSection />
       <ProjectsSection projects={projects} />
       <ContactSection />
     </>
